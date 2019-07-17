d51b47d962fdb8c28515376ec31e2fb83e9cb8a0
zhangjie <zhangjie@yunfucloud.com>
2019-07-17 18:17:57 +0800

    'index'

--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import "./home.less"
 import { SearchBar } from 'antd-mobile';
 import { Badge } from 'antd-mobile';
+import {Modal} from "antd-mobile";
+import { Tag } from 'antd-mobile';
+import { Button } from "antd-mobile";
 
 const Fragment = React.Fragment
 class Home extends Component {
@@ -11,6 +14,8 @@ class Home extends Component {
             val: 3,
             val1: 2,
             sign: false,
+            discountModal:false,
+            goodModal:false,
         };
 
     }
@@ -23,9 +28,18 @@ class Home extends Component {
         this.setState({ val1 });
     }
     handleColor =()=>{
-        this.setState({ sign: !this.state.sign });
+        this.setState({ 
+            sign: !this.state.sign,
+            goodModal:true,
+         });
+        
     }
-
+    showDiscountModal =(e)=>{
+        this.setState({ discountModal: true });
+    }
+    closeDiscountModal = () => {
+        this.setState({ discountModal: false });
+    }
     render() {
         return (
             <Fragment>
@@ -42,7 +56,7 @@ class Home extends Component {
                             <div className='commonInfo smallCommonInfo'>
                                 <div className='tag'>首</div>
                                 <div className='espText'> 新用户下单立减20元(不可与其他活动同享)</div>
-                                <div className='more'>共<span className='red'>5</span>个活动</div>
+                                <div className='more' onClick={this.showDiscountModal}>共<span className='red'>5</span>个活动</div>
                             </div>
                         </div>
                     </div>
@@ -110,6 +124,128 @@ class Home extends Component {
                         去结算
                 </div>
                 </div>
+
+                //优惠活动弹窗
+                <Modal
+                    visible={this.state.discountModal}
+                    transparent
+                    maskClosable={true}
+                    className='disCountModalWrap'
+                    footer={[]}
+                    onClose={this.closeDiscountModal}
+                >
+                    <div style={{ maxHeight: 400, overflow: 'scroll' }}>
+                         <div className='h1'>
+                            优惠
+                         </div>
+
+                         <div className='discountItem'>
+                            <Badge text="满减" 
+                                size={'small'}
+                            style={{
+                                padding: '0 3px',
+                                backgroundColor: '#fde6d2',
+                                borderRadius: 2,
+                                color: '#f37b1d',
+                                border: '1px solid #fde6d2',
+                                marginRight:'5px'
+                            }} />
+
+                            满29元减10元,满40元减去20元，
+                         </div>
+
+                        <div className='discountItem'>
+                            <Badge text="会员"
+                                size={'small'}
+                                style={{
+                                    padding: '0 3px',
+                                    backgroundColor: '#d7f0db',
+                                    borderRadius: 2,
+                                    color: '#39b54a',
+                                    border: '1px solid #d7f0db',
+                                    marginRight: '5px'
+                                }} />
+
+                            满29元减10元,满40元减去20元，
+                         </div>
+                        <div className='discountItem'>
+                            <Badge text="特价"
+                                size={'small'}
+                                style={{
+                                    padding: '0 3px',
+                                    backgroundColor: '#cce6ff',
+                                    borderRadius: 2,
+                                    color: '#0081ff',
+                                    border: '1px solid #cce6ff',
+                                    marginRight: '5px'
+                                }} />
+
+                            满29元减10元,满40元减去20元，
+                         </div>
+                        <div className='discountItem'>
+                            <Badge text="首单"
+                                size={'small'}
+                                style={{
+                                    padding: '0 3px',
+                                    backgroundColor: '#ede1d9',
+                                    borderRadius: 2,
+                                    color: '#a5673f',
+                                    border: '1px solid #ede1d9',
+                                    marginRight: '5px',
+                                }} />
+
+                            满29元减10元,满40元减去20元，
+                         </div>
+
+                        <div className='h1'>
+                            服务
+                         </div>
+                        <div className='discountItem'>
+                            送货上门到家
+                         </div>
+                        <div className='h1'>
+                                公告
+                         </div>
+                        <div className='discountItem'>
+                            本店买就送啤酒饮料瓜子花生
+                         </div>
+
+                    </div>
+                </Modal>
+
+                //商品选择弹窗
+
+                <Modal
+                    visible={this.state.goodModal}
+                    transparent
+                    maskClosable={true}
+                    className='goodModalWrap'
+                    footer={[]}
+                    onClose={this.closeDiscountModal}
+                >
+                    <div style={{ maxHeight: 400, overflow: 'scroll' }}>
+                          <div className='h1'>肯德基/汉堡</div>
+
+                          <div className='h2'>规格</div>
+
+                          <div className='tagContainer'>
+                            <Tag data-seed="logId">大汉堡</Tag>
+                            <Tag data-seed="logId">大汉堡</Tag>
+                            <Tag data-seed="logId">大汉堡</Tag>
+                            <Tag data-seed="logId">大汉堡</Tag>
+                            <Tag data-seed="logId">大汉堡</Tag>
+                            <Tag data-seed="logId">大汉堡</Tag>
+
+                        </div>
+
+                        <div className='goodFooter'>
+                            <div className='price'>￥18.5</div>
+                                <Button type="primary" inline size="small">选好了</Button>
+                        </div>
+                    </div>
+
+                    <div className='closeX'></div>
+                </Modal>
             </Fragment>
         )
     }
